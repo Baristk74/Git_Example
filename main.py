24746776a293ccadee0b2fad945912f3027b6ef0
Baristk74 <128810694+Baristk74@users.noreply.github.com>
2023-05-19 03:17:58 +0300

    First phase

---
+++ main.py
@@ -0,0 +1,8 @@
+
+import arsa
+import daire
+
+while True:
+    tur_secimi = input("Arsa fiyatı hesaplamak için 'a' daire fiyatı hesaplamak için 'd' tuşunu seçiniz! \n")
+    while tur_secimi !="a" and tur_secimi !="d":
+        tur_secimi = input("'a' ya da 'd' seçebilirsiniz!\n")
