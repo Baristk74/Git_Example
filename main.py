141833a71447bfa14fb62465fa4208302d6498ec
Baristk74 <128810694+Baristk74@users.noreply.github.com>
2023-05-19 03:30:42 +0300

    Programın sonlanmasının denetlenmesi
    klavyeden girilen karaktere göre programın ilerleyişi belirleyen kod bloğu

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ while True:
     tur_secimi = input("Arsa fiyatı hesaplamak için 'a' daire fiyatı hesaplamak için 'd' tuşunu seçiniz! \n")
     while tur_secimi !="a" and tur_secimi !="d":
         tur_secimi = input("'a' ya da 'd' seçebilirsiniz!\n")
+        denetleme = input("tekrar hesaplamak için e/E,çıkmak için h/H seçiniz \n")
+        while denetleme != "e" and denetleme != "E" and denetleme != "h" and denetleme != "H":
+            denetleme = input("e/E ya da h/H seçiniz!!\n")
